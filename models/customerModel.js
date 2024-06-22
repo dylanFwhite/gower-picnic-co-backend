7115b82e12d3f81d274f1293082a979bfc1937f8
Dylan White <white.dylan@live.co.uk>
2024-06-22 11:19:16 +0100

    Add data models

---
+++ models/customerModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const customerSchema = new mongoose.Schema(
+    {
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        name: {
+            type: String,
+            required: [true, 'Please provide your name!']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please provide your email'],
+            unique: true,
+            lowercase: true,
+            validate: [validator.isEmail, 'Please provide a valid email']
+        },
+        phone: {
+            type: String,
+            required: true,
+            validate: [validator.isMobilePhone, 'Please provide a valid mobile number']
+        },
+        orders: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'Order'
+            }
+        ]
+    }
+)
+
+export default mongoose.model('Customer', customerSchema)
---
+++ models/orderModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        orderDate: {
+            type: Date,
+            required: true
+        },
+        customer: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Customer'
+        },
+        products: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'Product'
+            }
+        ],
+        price: {
+            type: Number,
+            require: [true, 'Booking must have a price.']
+        },
+        paid: {
+            type: Boolean,
+            default: true
+        },
+        notes: {
+            type: String,
+            trim: true
+        }
+    }
+)
+
+export default mongoose.model('Order', orderSchema)
