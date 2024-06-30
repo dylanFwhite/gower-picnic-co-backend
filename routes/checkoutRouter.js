5478fa10d64ed2309205cca53debe648a1f7f1d4
Dylan White <white.dylan@live.co.uk>
2024-06-30 20:42:38 +0100

    Fix typo

--- routes/checkoutRouter.js
+++ routes/checkoutRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import {
   createCheckoutSession,
   getSessionStatus,
-} from "../controllers/checkoutController";
+} from "../controllers/checkoutController.js";
 
 const router = Router();
 
