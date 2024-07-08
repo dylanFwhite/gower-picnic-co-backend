1da81140d3d819170339ac6ac8ec31c048dc4ece
Dylan White <white.dylan@live.co.uk>
2024-07-08 20:31:30 +0100

    Customer create customer controller

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,11 +1,34 @@
 import Customer from "../models/customerModel.js";
+import catchAsync from "../utils/catchAsync.js";
 import factory from "./handlerFactory.js";
 
 const getAllCustomers = factory.getAll(Customer);
 
 const getCustomer = factory.getOne(Customer, { path: "orders" });
 
-const createCustomer = factory.createOne(Customer);
+const createCustomer = catchAsync(async (req, res) => {
+  const { email } = req.body;
+  const customer = await Customer.findOne({ email });
+  if (customer) {
+    const { _id } = customer;
+    return res.status(200).json({
+      status: "success",
+      data: {
+        _id,
+        email,
+      },
+    });
+  }
+  const newCustomer = await Customer.create(req.body);
+  const { _id } = newCustomer;
+  res.status(201).json({
+    status: "success",
+    data: {
+      _id,
+      email,
+    },
+  });
+});
 
 const updateCustomer = factory.updateOne(Customer);
 
